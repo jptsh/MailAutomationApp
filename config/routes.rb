910bf2f7e00a97cfa0b8c1436adc07b3acc3887d
jean pierre tshimanga <mail.jp@protonmail.com>
2020-09-24 21:57:02 +0200

    Set the versioning namespaces for API

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :audiences
 
   namespace :api, defaults: { format: :json } do 
-  
+    namespace :v1 do
+      # We are going to list our resources here
+    end
   end
 end
