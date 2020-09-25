27f4679035c31ca996293e3ddb5ca77af0d06f7e
jean pierre tshimanga <mail.jp@protonmail.com>
2020-09-25 12:59:46 +0200

    Adds the audiences-api create, update and destroy actions

--- app/controllers/api/v1/audience_controller.rb
+++ app/controllers/api/v1/audience_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::AudienceController < ApplicationController
+    before_action :set_audience, only: %i[show update destroy]
+    before_action :check_login, only: %i[create]
+    before_action :check_owner, only: %i[update destroy]
+
 
     def index
         render json: Audience.all
@@ -6,5 +10,50 @@ class Api::V1::AudienceController < ApplicationController
 
     def show
         render json: Audience.find(params[:id])
+    end
+
+    def create
+        audience = current_user.audiences.build(audience_params)
+        if audience.save
+            render json: audience, status: :created
+        else
+            render json: { errors: audience.errors }, status:
+            :unprocessable_entity
+        end
+    end
+
+    def update
+        if @audience.update(audience_params)
+        render json: @audience
+        else
+        render json: @audience.errors, status:
+        :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @product.destroy
+        head 204
+    end
+
+    private
+
+    def audience_params
+        params.require(:audience).permit(
+        :firstname, 
+        :lastname, 
+        :address, 
+        :phone,
+        :birthday,
+        :tags,
+        :email_marketing)
+    end
+
+    def check_owner
+        head :forbidden unless @audience.user_id == current_user&.id
+    end
+
+    def set_audience
+        @audience = Audience.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
-      resources :audiences, only: %i[show index]
+      resources :audiences
     end
   end
 end
