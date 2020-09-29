b3c297f72677146211a2c45eb8125aa3630879d9
jean pierre tshimanga <mail.jp@protonmail.com>
2020-09-29 19:47:08 +0200

    api base

--- app/controllers/api/v1/audiences_controller.rb
+++ app/controllers/api/v1/audiences_controller.rb
@@ -1,14 +1,35 @@
 class Api::V1::AudiencesController < ApplicationController
-    skip_before_action :authenticate_user!, only: :index
-
+    skip_before_action :verify_authenticity_token
+    skip_before_action :authenticate_user!, only: %i[index create]
+    before_action :validate_api_key!
     def index
         render json: Audience.all
     end
     
-    def show
-        render json: Audience.find(params[:id])
+    # def show
+    #     render json: Audience.find(params[:id])
+    # end
+
+    def create
+        audience = Audience.new(audience_params)
+        if audience.save
+            render json: audience, status: :created
+        else
+            render json: { errors: audience.errors }, status: :unprocessable_entity
+        end
     end
 
-    
+    private 
 
+    def audience_params
+        params.require(:audience).permit(:firstname, :lastname, :address, :phone, :birthday, :tags, :email_marketing)
+    end
+
+    def has_valid_api_key?
+       request.headers['X-Api-Key'] = "abc123"
+    end
+
+    def validate_api_key!
+        return head :forbidden unless has_valid_api_key?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # We are going to list our resources here
-      resources :audiences, only: %i[show index create]
+      resources :audiences, only: %i[index create]
     end
   end
 end
